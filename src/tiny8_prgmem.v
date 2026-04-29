adcd249b9759df0916b7b98d868f1fcfccf2ca46
luisjavierB0 <luis.bustamante@utp.edu.co>
2026-04-28 20:35:57 -0500

    Update tiny8_prgmem.v

--- src/tiny8_prgmem.v
+++ src/tiny8_prgmem.v
@@ -1,5 +1,3 @@
-`timescale 1ns/1ps
-
 module tiny8_prgmem (
     input  wire        clk,
     input  wire        rst_n,
@@ -16,7 +14,7 @@ module tiny8_prgmem (
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
             for (i = 0; i < 32; i = i + 1)
-                mem[i] <= 16'h0000; // NOP
+                mem[i] <= 16'h0000;
         end else if (wr_en) begin
             mem[wr_addr] <= wr_data;
         end
